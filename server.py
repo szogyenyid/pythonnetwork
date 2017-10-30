9e11299caad7d525edba2f8ff36504045db79a28
szogyenyid <szogyenyid@gmail.com>
2017-10-30 15:36:02 +0100

    Created server
    Basic server with port of choice

---
+++ server.py
@@ -0,0 +1,15 @@
+import socket
+serversocket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+print("Socket created")
+host = input("Please enter your ip address: ")
+port = int(input("The port you would like to use: "))
+serversocket.bind((host, port))
+print("Socket bound to %s" % str((host,port)))
+serversocket.listen(5)
+
+while True:
+	clientsocket,addr = serversocket.accept()      
+	print("Got a connection from %s" % str(addr))
+	msg = 'Thank you for connecting'+ "\r\n"
+	clientsocket.send(msg.encode('ascii'))
+	clientsocket.close()
