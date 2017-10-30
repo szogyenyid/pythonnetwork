728c803a2351f59472b1cd2c42e9280bdb6cd0be
szogyenyid <szogyenyid@gmail.com>
2017-10-30 18:34:59 +0100

    IP forced localhost, added ToDo

--- server.py
+++ server.py
@@ -5,16 +5,45 @@ def makeConnection():
 	global serversocket
 	serversocket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 	print("Socket created")
-	host = input("Please enter your ip address: ")
+	#host = input("Please enter your ip address: ")
 	port = int(input("The port you would like to use: "))
-	serversocket.bind((host, port))
-	print("Socket bound to %s" % str((host,port)))
+	serversocket.bind(('localhost', port))
+	print("Socket bound to %s" % str(('localhost',port)))
 	serversocket.listen(5)
 	print("Listener initialized")
 
+class connectionThread (threading.Thread):
+	def __init__(self):
+		threading.Thread.__init__(self)
+	def run(self):
+		print("Connections thread initialized.")
+	
+class processThread (threading.Thread):
+	def __init__(self):
+		threading.Thread.__init__(self)
+	def run(self):
+		print("Processing thread initialized.")
+	
+class listenThread (threading.Thread):
+	def __init__(self):
+		threading.Thread.__init__(self)
+	def run(self):
+		print("Listening thread initialized.")
+
+
+	
+
 makeConnection()
 #doing stuff
-while True:
+#Have to do:
+#Listen to new connections, and manage users -> connectionThread
+#Get username
+#Lisiten to new messages -> listenThread
+#Send new messages to everyone but sender -> processThread
+#change username -> processThread
+#
+running = True
+while running:
 	clientsocket,addr = serversocket.accept()      
 	print("Got a connection from %s" % str(addr[0]))
 	msg = 'SERVER: Thank you for connecting'+ "\r\n"
