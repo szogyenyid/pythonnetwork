bdfa2936deb463011d63b3ad92255072037ce1da
szogyenyid <szogyenyid@gmail.com>
2017-10-30 17:05:44 +0100

    Encapsulating functions

--- client.py
+++ client.py
@@ -1,10 +1,21 @@
 import socket
-clientsocket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-print("Socket created")
-host = input("Please enter the IP you would like to connect to: ")
-port = int(input("The port you would like to use: "))
-clientsocket.connect((host,port))
-print("Connected to %s" % str((host,port)))
+
+def makeConnection():
+	global clientsocket
+	clientsocket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+	print("Socket created")
+	host = input("Please enter the IP you would like to connect to: ")
+	port = int(input("The port you would like to use: "))
+	clientsocket.connect((host,port))
+	print("Connected to %s" % str((host,port)))
+	
+def closeConnection():
+	clientsocket.close()
+	
+makeConnection()
+#doing stuff
 msg = clientsocket.recv(1024)
-print ("Message from server:" + msg.decode('ascii'))
-clientsocket.close()
+print ("Message from server: " + msg.decode('ascii'))
+#doing stuff ends
+closeConnection()
+dummy = input("Quit")
