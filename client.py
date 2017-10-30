33eda4fcc8fa8493c8e9f37a352080aa4119eded
szogyenyid <szogyenyid@gmail.com>
2017-10-30 15:44:24 +0100

    Created client
    Cleint can connect to any IP on port of choice

---
+++ client.py
@@ -0,0 +1,10 @@
+import socket
+clientsocket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+print("Socket created")
+host = input("Please enter the IP you would like to connect to: ")
+port = int(input("The port you would like to use: "))
+clientsocket.connect((host,port))
+print("Connected to %s" % str((host,port)))
+msg = clientsocket.recv(1024)
+print ("Message from server:" + msg.decode('ascii'))
+clientsocket.close()
